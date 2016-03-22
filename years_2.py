a6227138634784ebc49d4b0f1bf6709843560156
Michael Quintero <michael.quintero@rackspace.com>
2016-03-22 17:37:05 -0500

    Added more notations within the script

--- years_2.py
+++ years_2.py
@@ -31,3 +31,4 @@ print sayHello, sayAge
 
 ## NOTES: We introduced two new built-in functions into the script. These functions are built into python and are always available. They are len($) and raw_input().
 ## The int() function will return an integer object constructed from a number or string x, or return 0 if no arguments are given. If x is a number, it can be a plain integer, a long integer, or a floating point number. If x is floating point, the conversion truncates towards zero. If the argument is outside the integer range, the function returns a long object instead.
+## https://docs.python.org/2/library/functions.html
